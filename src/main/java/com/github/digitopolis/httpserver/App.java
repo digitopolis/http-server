6139b19eb12eb59b7a0a8bbafeea81e765ccfb4a
Matt Readout <matt.readout@gmail.com>
2020-04-29 13:59:04 -0700

    Add InputValidator for port number to confirm valid input, otherwise use default port

--- src/main/java/com/github/digitopolis/httpserver/App.java
+++ src/main/java/com/github/digitopolis/httpserver/App.java
@@ -1,10 +1,12 @@
 package com.github.digitopolis.httpserver;
 
 import com.github.digitopolis.httpserver.cli.CLI;
+import com.github.digitopolis.httpserver.validator.InputValidator;
 
 public class App
 {
     private static final CLI cli = new CLI();
+    private static final InputValidator inputValidator = new InputValidator();
 
     public static void main( String[] args )
     {
@@ -14,7 +16,7 @@ public class App
     public static void startServer() {
         cli.printMessage("Enter a port number to start an echo server:");
         String port = cli.getInput();
-        Server server = new Server(Integer.parseInt(port));
+        Server server = new Server(inputValidator.validatePort(port));
         server.start();
     }
 }
