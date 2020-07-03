f293bd43214d8f494fe98075822a37bff20626e1
Matt Readout <matt.readout@gmail.com>
2020-07-02 17:28:36 -0700

    Add case for GET request with HTML body to Router

--- src/main/java/com/github/digitopolis/httpserver/router/Router.java
+++ src/main/java/com/github/digitopolis/httpserver/router/Router.java
@@ -7,6 +7,11 @@ public class Router {
     public static HTTPResponse handleRequest(HttpRequest request) {
         switch (request.path) {
             case "/simple_get": return new HTTPResponse(request.httpVersion, "200", "OK");
+            case "/simple_get_with_body":
+                HTTPResponse response = new HTTPResponse(request.httpVersion, "200", "OK");
+                response.addContentType("Content-Type: text/html");
+                response.addBody("Hello world!");
+                return response;
             default: return new HTTPResponse(request.httpVersion, "404", "Not Found");
         }
     }
