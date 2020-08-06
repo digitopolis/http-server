3a340ce72918a654632493b613584c6a7bde14dd
Matt Readout <matt.readout@gmail.com>
2020-08-05 17:52:43 -0700

    Update RouterTest to take HttpRequest instead of String

--- src/test/java/com/github/digitopolis/httpserver/router/RouterTest.java
+++ src/test/java/com/github/digitopolis/httpserver/router/RouterTest.java
@@ -1,6 +1,7 @@
 package com.github.digitopolis.httpserver.router;
 
 import com.github.digitopolis.httpserver.parser.RequestParser;
+import com.github.digitopolis.httpserver.request.HttpRequest;
 import org.junit.jupiter.api.Test;
 
 import com.github.digitopolis.httpserver.response.HTTPResponse;
@@ -10,8 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RouterTest {
     @Test
     public void testSends200ResponseToValidPath() {
-        String validRequest = "GET /simple_get HTTP/1.1";
-        HTTPResponse response = Router.handleRequest(RequestParser.parseInput(validRequest));
+        HttpRequest validRequest = new HttpRequest("GET","/simple_get","HTTP/1.1");
+        HTTPResponse response = Router.handleRequest(validRequest);
         assertEquals("200", response.statusCode);
     }
 }
